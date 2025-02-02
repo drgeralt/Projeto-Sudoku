7ed642f3a0a61cec8f30b92398a016761bbdc84e
DrGeralt <rhyannsousa@gmail.com>
2025-02-01 21:32:28 -0300

    2 commit yay

--- Scripts/Solver.py
+++ Scripts/Solver.py
@@ -1,1 +1,6 @@
-def solve(board):
+numbers = [1,2,3,4,5,6,7,8,9]
+def solve_line(sudoku):
+    for elt in sudoku:
+        if elt ==0:
+            tries = numbers
+            for line_buddy in sudoku[][elt.inde]
